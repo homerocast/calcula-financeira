3e914dfd1a9014f3531c076b97bf19e74087981a
homerocast <homero.cast@gmail.com>
2026-08-08 14:11:14 -0300

    Privacidade

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,17 @@
+(function () {
+  var CHAVE = 'calculafin_cookie_consent';
+  if (localStorage.getItem(CHAVE)) return;
+
+  var barra = document.createElement('div');
+  barra.className = 'cookie-banner';
+  barra.innerHTML =
+    '<span>Usamos cookies para exibir anúncios e entender o uso do site. ' +
+    'Saiba mais na <a href="/privacidade.html">Política de Privacidade</a>.</span>' +
+    '<button type="button" class="btn-stamp" id="cookieAceitar">Entendi</button>';
+  document.body.appendChild(barra);
+
+  document.getElementById('cookieAceitar').addEventListener('click', function () {
+    localStorage.setItem(CHAVE, '1');
+    barra.remove();
+  });
+})();
